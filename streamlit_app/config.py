7e45ded09b5842c30d7aacf443b8bf9efea7c8d1
AlexRandria <51092539+AlexRandria@users.noreply.github.com>
2023-04-25 17:46:08 +0200

    Ajout des configs

--- streamlit_app/config.py
+++ streamlit_app/config.py
@@ -7,15 +7,30 @@ Config file for Streamlit App
 from member import Member
 
 
-TITLE = "My Awesome App"
+TITLE = "Projet NBA"
 
 TEAM_MEMBERS = [
     Member(
-        name="John Doe",
-        linkedin_url="https://www.linkedin.com/in/charlessuttonprofile/",
-        github_url="https://github.com/charlessutton",
+        name="Leo Ferretti",
+        linkedin_url="https://www.linkedin.com/in/leo-ferretti-18042724b",
+        github_url="https://github.com/LeoF57/",
+    ),
+    Member(
+        name="Florian Lluch",
+        linkedin_url="https://www.linkedin.com/in/florian-lluch/",
+        github_url="https://github.com/Lluchi/",
+    ),
+    Member(
+        name="Alex Randrianantoandro",
+        linkedin_url="https://www.linkedin.com/in/alex-randrianantoandro-8935ab179/",
+        github_url="https://github.com/AlexRandria",
+    ),
+    Member(
+        name="Fabien Vidor",
+        linkedin_url="https://www.linkedin.com/in/fabien-vidor/",
+        github_url="https://github.com/Fabien-Vidor/",
     ),
     Member("Jane Doe"),
 ]
 
-PROMOTION = "Promotion Bootcamp Data Scientist - April 2021"
+PROMOTION = "Promotion Bootcamp Data Scientist - February 2023"
