f5b986b23e1508b4f67c8bdfc42a6fd5a5700f06
AlexRandria <51092539+AlexRandria@users.noreply.github.com>
2023-05-02 15:35:56 +0200

    Ajout des infos sur les profils

--- streamlit_app/tabs/Presentation.py
+++ streamlit_app/tabs/Presentation.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from member import Member
+import config
 
 title = "Qui sommes-nous ?"
 sidebar_name = "Qui sommes-nous ?"
@@ -10,27 +10,37 @@ def run():
     
     tab1, tab2, tab3, tab4 = st.tabs(["Leo Ferretti", "Florian Lluch", "Alex Randrianantoandro", "Fabien Vidor"])
     with tab1:
+        st.header("Contact :")
+        st.write(config.TEAM_MEMBERS[0].presentation_markdown(), unsafe_allow_html=True)
         st.markdown("""
                     """)
-        
+
     with tab2:
+        st.header("Contact :")
+        st.write(config.TEAM_MEMBERS[1].presentation_markdown(), unsafe_allow_html=True)
         st.markdown(
             """
                
             """
         )
         
-   
 
     with tab3:
+        st.header("Contact :")
+        st.write(config.TEAM_MEMBERS[2].presentation_markdown(), unsafe_allow_html=True)
+        
         st.markdown(
             """
                
             """
         )
    
+        
    
     with tab4:
+        st.header("Contact :")
+        st.write(config.TEAM_MEMBERS[3].presentation_markdown(), unsafe_allow_html=True)
+        
         st.markdown(
             """
             De formation mathématiques (licence en mathématiques option informatique) j'ai ensuite fait carrière à l'Armée de Terre en tant que sous-officier.
